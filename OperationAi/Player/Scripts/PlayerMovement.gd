4e5cf77c2163aaecf2795d51e1a5d04385bc27e2
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-05-01 00:00:53 +0100

    Add Left and Right Movement

--- OperationAi/Player/Scripts/PlayerMovement.gd
+++ OperationAi/Player/Scripts/PlayerMovement.gd
@@ -1,16 +1,86 @@
 extends Node2D
 
+var player
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2.ZERO
+var UP = Vector2(0, -1)
+
+#Base Variables
+export var BASE_ACEL = 25
+export var BASE_MAX_VEL = 130
+export var BASE_DECEL = 0.6
 
 
-# Called when the node enters the scene tree for the first time.
+var acel 
+var maxVel
+var decel
+
+
 func _ready():
-	pass # Replace with function body.
+	ResetBaseValues()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func Init(owner):
+	player = owner
+	
+	
+func ResetBaseValues():
+	acel = BASE_ACEL
+	maxVel = BASE_MAX_VEL
+	decel = BASE_DECEL
+
+
+func GetInput():
+	if Input.is_action_pressed("LEFT"):
+		#Turn Sprite
+		#player.playerSprite.TurnLeft()
+		
+		#Set Y Velocity To 0
+		ChangeVelocityY(0,0,false, false)
+		
+		#Set Velocity
+		if velocity.x > 0: ChangeVelocityX(decel, -acel, false, true)
+		else: ChangeVelocityX(-acel, maxVel, true)
+	elif Input.is_action_pressed("RIGHT"):
+		#Turn Sprite
+		#player.playerSprite.TurnRight()
+		
+		#Set Y Velocity To 0
+		ChangeVelocityY(0,0,false, false)
+		
+		#Set Velocity
+		if -velocity.x > 0: ChangeVelocityX(decel, acel, false, true)
+		else: ChangeVelocityX(acel, maxVel, true)
+	else:
+		ChangeVelocityX(decel, 0, false, true)
+
+
+func _physics_process(_delta):
+	GetInput()
+	player.move_player(velocity, UP)
+
+
+func ChangeVelocityX(value, maxVelocity=0, adding=true, gradual=false):
+	""" adding -> if you want to just set value or add it to current velocity
+		gradual -> will lerp the velocity to maxVelocity with weight value"""
+	if!gradual:
+		if adding: 
+			velocity.x += value
+		else: velocity.x = value
+		
+		if maxVelocity: velocity.x = clamp(velocity.x, -maxVelocity, maxVelocity)
+	else: velocity.x = lerp(velocity.x, maxVelocity, value)
+	return velocity
+
+
+func ChangeVelocityY(value, maxVelocity=0, adding=true, gradual=false):
+	""" adding -> if you want to just set value or add it to current velocity
+		gradual -> will lerp the velocity to maxVelocity with weight value"""
+	if !gradual:
+		if adding: 
+			velocity.y += value
+		else: velocity.y = value
+		
+		if maxVelocity: velocity.y = clamp(velocity.y, -maxVelocity, maxVelocity)
+	else: velocity.y = lerp(velocity.y, maxVelocity, value)
+	return velocity
