65c78f7848c16069c8bf6d1ef2d7119a66f4b3e7
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-05-01 00:56:51 +0100

    Create MainMenu PlaceHolder

--- OperationAi/Scripts/WorldManager.gd
+++ OperationAi/Scripts/WorldManager.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 onready var roomScene = load("res://Scenes/Room.tscn")
+onready var mainMenuScene = load("res://Scenes/Menus/MainMenu.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+########################
+#MAIN GENERAL FUNCTIONS#
+########################
 
 func SwitchScene(currentScene, newScene):
 	#Take out the currentScene
@@ -15,3 +20,13 @@ func SwitchScene(currentScene, newScene):
 func AddSceneInstance(scene):
 	var sceneInstance = scene.instance()
 	add_child(sceneInstance)
+	
+########################
+#SPECIFIC SCENE CHANGES#
+########################
+
+func SwitchToRoom(currentScene):
+	SwitchScene(currentScene, roomScene)
+	
+func SwitchToMainMenu(currentScene):
+	SwitchScene(currentScene, mainMenuScene)
