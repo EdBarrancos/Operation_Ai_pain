2337ce3f8a577471ae231125e723bf4634dfb4a9
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-05-01 16:46:33 +0100

    Fix PLayerCamera | Create (NotWorking) Vignette Shader

--- OperationAi/Player/Scripts/Player.gd
+++ OperationAi/Player/Scripts/Player.gd
@@ -4,15 +4,26 @@ onready var playerSprite = $PlayerSprite
 onready var playerMovement = $PlayerMovement
 onready var playerCollisonShape = $PlayerCollisionShape
 onready var playerSoupInventory = $SoupInventory
+onready var playerCamera = $PlayerCamera2D
 
 #DEBUGS
 export var DMOVEMENT = false
 export var DINVETORY = false
+export var DCAMERA = false
+
+#Camera Limits
+export var upperLimit = 40
+export var lowerLimit = 300
+export var leftLimit = -275
+export var rightLimit = 275
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	playerMovement.Init(self, DMOVEMENT)
 	playerSoupInventory.Init(self, DINVETORY)
+	playerCamera.Init(self, DCAMERA)
+	playerCamera.SetLimits(upperLimit, lowerLimit, leftLimit, rightLimit)
 
 func _process(delta):
 	if Input.is_action_just_pressed("TEST"):
--- OperationAi/Player/Scripts/PlayerCamera2D.gd
+++ OperationAi/Player/Scripts/PlayerCamera2D.gd
@@ -1,31 +1,19 @@
 extends Camera2D
 
-export var playerPath: NodePath
 var player
-
-export var upperLimit = 300
-export var lowerLimit = -300
-export var rightLimit = 168
-export var leftLimit = -168
+var DEBUG
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node(playerPath)
+	pass
+	
+func Init(owner, debug):
+	player = owner
+	DEBUG = debug
+
+func SetLimits(upperLimit, lowerLimit, leftLimit, rightLimit):
 	limit_bottom = lowerLimit
 	limit_top = upperLimit
 	limit_left = leftLimit
 	limit_right = rightLimit
-
-
-func _process(delta):
-	Follow()
-
-func Follow():
-	global_position = player.global_position
-	
-func UpdateX(value):
-	global_position.x = value
-	
-func UpdateY(value):
-	global_position.y = value
