0b291a2488a467d75b784b926e3650fd8c22179d
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-05-01 13:19:33 +0100

    Add CameraLimits

--- OperationAi/Player/Scripts/PlayerCamera2D.gd
+++ OperationAi/Player/Scripts/PlayerCamera2D.gd
@@ -3,7 +3,7 @@ extends Camera2D
 export var playerPath: NodePath
 var player
 
-export var uperLimit = 300
+export var upperLimit = 300
 export var lowerLimit = -300
 export var rightLimit = 168
 export var leftLimit = -168
@@ -12,6 +12,10 @@ export var leftLimit = -168
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node(playerPath)
+	limit_bottom = lowerLimit
+	limit_top = upperLimit
+	limit_left = leftLimit
+	limit_right = rightLimit
 
 
 func _process(delta):
@@ -19,3 +23,9 @@ func _process(delta):
 
 func Follow():
 	global_position = player.global_position
+	
+func UpdateX(value):
+	global_position.x = value
+	
+func UpdateY(value):
+	global_position.y = value
