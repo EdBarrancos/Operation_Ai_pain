f010f061c6528dcf79522b3c716feeb31cb6beef
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-05-02 16:21:14 +0100

    Add Walking Sound

--- OperationAi/Player/Scripts/PlayerAnimation.gd
+++ OperationAi/Player/Scripts/PlayerAnimation.gd
@@ -6,7 +6,12 @@ var playerMovement
 var playerSprite
 var stopAnimation = false
 
+var walking = false
+var startWalking = false
+var continueWalking = false
+
 onready var animationPlayer = $AnimationPlayer
+onready var footsteps = $FootSteps
 
 
 # Called when the node enters the scene tree for the first time.
@@ -39,7 +44,32 @@ func turn_left():
 	if not is_sprite_flipped(): flip_sprite()
 		
 func PlayWalkingAnimation():
+	walking = true
+	UpdateWalking()
 	animationPlayer.play("Walking")
+	PlayFootstepSound()
 	
 func StopAnimation():
+	walking = false
+	UpdateWalking()
 	animationPlayer.stop()
+	StopFootstepSound()
+	
+func PlayFootstepSound():
+	if startWalking:
+		footsteps.play()
+		
+func StopFootstepSound():
+	footsteps.stop()
+	
+	
+func UpdateWalking():
+	if walking:
+		startWalking = false
+	if not walking:
+		continueWalking = false
+	if walking and not startWalking and not continueWalking:
+		#Just Started
+		startWalking = true
+		continueWalking = true
+
