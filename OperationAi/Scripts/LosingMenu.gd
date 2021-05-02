fb558147870ac6f717fd987eb268f82650d18544
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-05-02 18:51:04 +0100

    Main Manehabcb

--- OperationAi/Scripts/LosingMenu.gd
+++ OperationAi/Scripts/LosingMenu.gd
@@ -21,8 +21,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_parent().SwitchToRoom(self)
+	if Input.is_action_just_pressed("ui_cancel") or Input.is_action_just_pressed("ui_accept"):
+		get_parent().SwitchToMainMenu(self)
 
 
 func _on_CutsceneTimer_timeout():
--- OperationAi/Scripts/MainMenu.gd
+++ OperationAi/Scripts/MainMenu.gd
@@ -4,14 +4,48 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var animationPlayer = $AnimationPlayer
 
+onready var animations = ["Load", "Loop", "Back"]
+onready var currentAnim = 0
+onready var dir = 1
+export var loopTime = 10
+onready var currentLoop = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	PlayAnimation()
 
+func NextAnimation():
+	if currentAnim + dir >= animations.size():
+		currentAnim = 0
+	else:
+		currentAnim += dir
+	print(currentAnim)
+	
+		
+		
+func PlayAnimation():
+	animationPlayer.play(animations[currentAnim])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_parent().SwitchToIntroCutscene(self)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	print("Finished")
+	if anim_name == "Loop":
+		if currentLoop >= loopTime:
+			NextAnimation()
+			PlayAnimation()
+			currentLoop = 0
+			return
+		PlayAnimation()
+		currentLoop += 1
+		return
+	NextAnimation()
+	PlayAnimation()
+			
+	
